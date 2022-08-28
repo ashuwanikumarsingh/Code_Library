27b75835cc187491997b48e1a33e9e6f5d4f2b68
Ashuwani Kumar Singh <99886096+ashuwanikumarsingh@users.noreply.github.com>
2022-08-28 19:52:21 +0530

    Create Prime.java

---
+++ Basic_Problems/Pep_Coding_Problems/Prime.java
@@ -0,0 +1,96 @@
+// 1. You've to check whether a given number is prime or not.
+// 2. Take a number "t" as input representing count of input numbers to be tested.
+// 3. Take a number "n" as input "t" number of times.
+// 4. For each input value of n, print "prime" if the number is prime and "not prime" otherwise.
+// Input Format
+// A number t
+// A number n
+// A number n
+// .. t number of times
+// Output Format
+// prime
+// not prime
+// not prime
+// .. t number of times
+
+
+// Constraints
+// 1 <= t <= 10000
+// 2 <= n < 10^9
+// Sample Input
+// 5
+// 13
+// 2
+// 3
+// 4
+// 5
+// Sample Output
+// prime
+// prime
+// prime
+// not prime
+// prime
+
+
+
+
+// First solution
+
+import java.util.*;
+  
+  public class Main{
+  
+  public static void main(String[] args) {
+      Scanner scn = new Scanner(System.in);
+    int t = scn.nextInt();
+
+    for (int i = 0; i < t; i++) {
+        System.out.println("A number "+t);
+      int n = scn.nextInt();
+      int counter = 0;
+      for (int div = 1; div <= n; div++) {
+        if (n % div == 0) {
+          counter++;
+        }
+      }
+
+      if (counter == 2) {
+        System.out.println("prime");
+      }
+      else {
+        System.out.println("not prime");
+      }
+    }
+  
+   }
+  }
+// Main Solution  Using square root methond in order to reduce the time complexty
+
+import java.util.*;
+  
+  public class Main{
+  
+  public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+    int t = scn.nextInt();
+
+    for (int i = 0; i < t; i++) {
+        int n = scn.nextInt();
+        int counter = 0;
+        
+        for (int div = 2; div*div <= n; div++) {
+            if (n % div == 0) {
+                counter++;
+            }
+        }
+
+        if (counter == 0) {
+            System.out.println("prime");
+        }
+        else {
+            System.out.println("not prime");
+        }
+    }
+  
+   }
+  }
