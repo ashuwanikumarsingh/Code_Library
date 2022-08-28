f42178065daece706623f930c4db4e69277fe528
Ashuwani Kumar Singh <99886096+ashuwanikumarsingh@users.noreply.github.com>
2022-08-28 11:29:11 +0530

    Update wap_to_check_n_numbersofPrime.java

--- Basic_Problems/wap_to_check_n_numbersofPrime.java
+++ Basic_Problems/wap_to_check_n_numbersofPrime.java
@@ -1,3 +1,12 @@
+// Question Link: 
+// https://nados.io/question/is-a-number...
+
+// Question Statement: 
+// 1. You've to check whether a given number is prime or not.
+// 2. Take a number "t" as input representing count of input numbers to be tested.
+// 3. Take a number "n" as input "t" number of times.
+// 4. For each input value of n, print "prime" if the number is prime and "not prime" otherwise.
+
 import java.util.*;
 
 public class Main {
