ed3c7787ceb609736302e3873816dc3ff62561a3
Ashuwani Kumar Singh <99886096+ashuwanikumarsingh@users.noreply.github.com>
2022-09-04 10:24:16 +0530

    Create Reverse A Number.java

---
+++ Basic_Problems/Pep_Coding_Problems/Reverse A Number.java
@@ -0,0 +1,55 @@
+// Reverse A Number
+// Easy
+
+// 1. You've to display the digits of a number in reverse.
+// 2. Take as input "n", the number for which digits have to be display in reverse.
+// 3. Print the digits of the number line-wise, but in reverse order.
+
+// Constraints
+// 1 <= n < 10^9
+
+// Format
+// Input
+// "n" where n is any integer.
+
+// Output
+// d1
+// d2
+// d3
+// ... digits of the number in reverse
+
+// Example
+// Sample Input
+
+// 65784383
+
+// Sample Output
+// 3
+// 8
+// 3
+// 4
+// 8
+// 7
+// 5
+// 6
+
+
+
+
+
+import java.util.*;
+   
+   public class Main{
+   
+   public static void main(String[] args) {
+     // write your code here 
+     Scanner sc = new Scanner(System.in);
+     int n = sc.nextInt();
+
+     while ( n>0 ) {
+       int b = n % 10;
+        n = n/10;
+        System.out.println(b);
+     }
+    }
+   }
